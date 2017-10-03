b3e0a3e6134d4f84dae5a419434312273f181b41
samgoodrick <sdg31@zips.uakron.edu>
2017-10-03 17:31:52 -0400

    begin translations of specific expressions

--- pip/translator.cpp
+++ pip/translator.cpp
@@ -8,3 +8,9 @@ translator::operator()(const sexpr::expr* e)
 		return translate_prog(l);
 	sexpr::throw_unexpected_term(e);
 }
+
+translator::translate_int_expr(const sexpr::int_expr* e)
+{
+	// TODO: Make this take an int_type pointer
+	return cxt.make_int_expr(nullptr, e->val)
+}
