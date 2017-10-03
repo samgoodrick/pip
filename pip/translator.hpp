73346781fefcbd4b411a7a5665b26ff08f95cf46
samgoodrick <sdg31@zips.uakron.edu>
2017-10-03 17:10:03 -0400

    Stubbed out translator class

--- pip/translator.hpp
+++ pip/translator.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
+#include <sexpr/translation.hpp>
+#include <pip/expr.hpp>
+#include <pip/context.hpp>
+
 // TODO: This module will define the sexpr translation.
 
 namespace pip
 {
-
+	class translator : public sexpr:translator<translator>
+	{		
+	public:
+		translator( context& cxt );
+		
+		expr* translate_expr(const sexpr::expr* e);
+		expr* translate_int_expr(const sexpr::int_expr* e);
+	private:
+		context& cxt;
+		
+	};
 
 
 } // namespace pip
